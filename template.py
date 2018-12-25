54bc938938d6295f740f8b215f5858de54908920
Robert Barnes <robert.raybarnes@gmail.com>
2018-12-25 20:39:39 +0000

    added logic to populate Params, storage and compute files

--- template.py
+++ template.py
@@ -1,5 +1,8 @@
-import click
 import os
+from os import write
+
+import click
+
 
 cwd = os.getcwd()
 
@@ -13,18 +16,85 @@ def main():
 def template(self, name, path):
     os.makedirs("{0}/{1}".format(path, name))
 
-    f=open("{0}/{1}/masterfile.yml".format(path, name), 'w+')
-    f.write("# This is the master file.  Don not edit directly")
+    f=open("{0}/{1}/masterfile.yml\n".format(path, name), 'w+')
+    f.write("# This is the master file.  Do not edit directly\n")
+    f.write('AWSTemplateFormatVersion: "2010-09-09"\n')
     f.close()
     
     files = ['parameters', 'storage', 'compute', 'database', 'security', 
     'blockchain', 'analytics', 'management', 'iot', 'media', 'network', 'machine-learning']
     
     for a in files:
-        d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
-        d.write("# This is the {0} file.".format(a))
-        d.close()
-
+        if a == 'parameters':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.\n".format(a))
+            d.write("# Store all cloudformation parameters in this file\n")
+            d.write("#\n")
+            d.write("# Parameters:\n")
+            d.close()
+        elif a == 'storage':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.\n".format(a))
+            d.write("# Describe all storage resources in this file.\n")
+            d.write("# Storage resources include\n")
+            d.write("# s3\n")
+            d.write("# EFS\n")
+            d.write("# FSx\n")
+            d.write("# s3 Glazier\n")
+            d.write("# Storage Gateway\n")
+            d.write("#\n")
+            d.write("# Resources:\n")
+            d.close()
+        elif a == 'compute':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.\n".format(a))
+            d.write("# Describe all compute resources in this file.\n")
+            d.write("# Compute resources include\n")
+            d.write("# EC2\n")
+            d.write("# Lightsail\n")
+            d.write("# ECR\n")
+            d.write("# ECS\n")
+            d.write("# EKS\n")
+            d.write("# Batch\n")
+            d.write("#\n")
+            d.write("# Resources\n")
+            d.close()
+        elif a == 'database':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
+        elif a == 'secuirty':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
+        elif a =='blockchain':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
+        elif a == 'analytics':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
+        elif a == 'management':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
+        elif a == 'iot':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
+        elif a == 'media':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
+        elif a == 'network':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
+        elif a == 'machine-learning':
+            d=open("{0}/{1}/{2}.yml".format(path, name, a), 'w+')
+            d.write("# This is the {0} file.".format(a))
+            d.close()
 
 if __name__ == '__main__':
     main()
